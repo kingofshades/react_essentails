030c3a93d145b181d6520cb45f1c1809398b3173
Armaghan <armaghanahmed05@gmail.com>
2025-03-19 14:14:46 +0500

    chore(ci): Github Actions (q)

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,8 @@ function App() {
   return (
     <div className="flex h-screen w-full justify-center items-center">
       <h1 className="text-4xl font-bold">Hello World</h1>
-      <p>Dummy User</p>
     </div>
-  )
+)
 }
 
 export default App
